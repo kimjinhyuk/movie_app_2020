8f4a1ddfb11bfa4fd4c5b886f2598d15ff5f2542
kimjinhyuk <kimjinhyuk2@gmail.com>
2020-10-16 04:40:26 +0900

    update movie app

---
+++ src/components/Navigation.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Navigation(){
+  return <div>
+    <a href="/">Home</a>
+    <a href="/about">about</a>
+  </div>
+}
+
+export default Navigation;
---
+++ src/router/About.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function About() {
+  return <span> About this page : i build it cuz i love movie </span>;
+}
+
+export default About;
